d71824f9c0eb80713edd7e98db86e2c4b1467db3
jinogr4 <jinog4@gmail.com>
2025-02-26 02:42:39 +0530

    Completed_DownloadAndVerify

---
+++ TrumpfMetamation_Task_Orbiter/Pages/DownloadPage.cs/FileDownload.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System.IO;
+using System.Threading;
+
+namespace TrumpfMetamation_Task_Orbiter.Pages.DownloadPage.cs
+{
+    public class FileDownload
+    {
+        public void DownloadAndDelete(IWebDriver driver, IReadOnlyList<IWebElement> Items)
+        {
+            string folder = @"C:\Users\jinog\Downloads\";
+
+            foreach (var Item in Items)
+            {
+                string downloadLink = Item.FindElement(By.TagName("a")).GetAttribute("href");
+                string fileName = Path.GetFileName(downloadLink);
+                string filePath = Path.Combine(folder, fileName);
+                driver.Navigate().GoToUrl(downloadLink);
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(200));
+                Thread.Sleep(500);
+
+                if (File.Exists(filePath))
+                {
+                    File.Delete(filePath);
+                    Console.WriteLine($"File {fileName} downloaded and deleted at: {filePath}");
+
+                }
+                else
+                {
+                     throw new FileNotFoundException($"File {fileName} did not download correctly at {filePath}");
+                }
+            }
+
+        }
+    }
+}
--- TrumpfMetamation_Task_Orbiter/UnitTest1.cs
+++ TrumpfMetamation_Task_Orbiter/UnitTest1.cs
@@ -1,3 +1,8 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using TrumpfMetamation_Task_Orbiter.Pages.DownloadPage.cs;
+
 namespace TrumpfMetamation_Task_Orbiter
 {
     public class Tests
@@ -8,9 +13,30 @@ namespace TrumpfMetamation_Task_Orbiter
         }
 
         [Test]
-        public void Test1()
+        public void OrbiterDownloadFileTest()
         {
-            Assert.Pass();
+            IWebDriver driver = new ChromeDriver();
+            //Why I used as Array , to avoid the Duplicate Lines of the Code , workouting the Iteration 
+            string[] urls = {
+                "https://orbiter-for-testing.azurewebsites.net/products/testApp?isInternal=false",
+                "https://orbiter-for-testing.azurewebsites.net/products/testApp?isInternal=true"
+            };
+            foreach (var url in urls)
+            {
+
+                driver.Navigate().GoToUrl(url);
+                IWebElement DownloadTestAppLbl = driver.FindElement(By.XPath("//h1[normalize-space(text())='Download TestApp']"));
+                Assert.True(DownloadTestAppLbl.Displayed, "Download TestApp is not displayed");
+
+                var listItemsTestApp = driver.FindElements(By.XPath("//ul[@class='list-group list-group-flush']/li"));
+                Console.WriteLine($"Files in List: {listItemsTestApp.Count}");
+
+                FileDownload fileDownload = new FileDownload();
+                fileDownload.DownloadAndDelete(driver, listItemsTestApp);
+                Thread.Sleep(500);
+            }
+
+            driver.Quit();
         }
     }
 }
