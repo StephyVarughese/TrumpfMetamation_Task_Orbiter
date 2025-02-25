a4e970e8d645d44248271ec094cab076f003bb7d
jinogr4 <jinog4@gmail.com>
2025-02-25 22:20:05 +0530

    Add project files.

---
+++ TrumpfMetamation_Task_Orbiter/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TrumpfMetamation_Task_Orbiter
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
